c64262c31a632015e316066b5a727a9e3aeb83a2
nabeel-w <hassanfarhat986@gmail.com>
2023-08-02 23:21:52 +0530

    tt

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose.connect(mongoLink, {useNewUrlParser: true});
 app.use("/api/auth",authRoute);
 app.use("/api/task",taskRoute);
 
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "./dist")));
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./dist/index.html"));
